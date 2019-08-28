84254334fdba27a6208e4ffd71a9860864d8941e
cvasqxz <cvxz@pm.me>
2019-08-28 11:44:44 -0400

    text array

--- tests/image_tests.py
+++ tests/image_tests.py
@@ -7,27 +7,31 @@ EPD_HEIGHT = EPD_WIDTH = 200
 def main():
     image = Image.new('1', (EPD_WIDTH, EPD_HEIGHT), 255)
     draw = ImageDraw.Draw(image)
-    font = ImageFont.truetype('../fonts/Comfortaa-Regular.ttf', 11)
+    font = ImageFont.truetype('../fonts/Comfortaa-Light.ttf', 12)
 
-    draw.text((14, 24), 'ABCČĆDĐEFGHIJKLMNOPQR', font= font, fill= 0)
-    draw.text((14, 36), 'SŠTUVWXYZŽabcčćdđefghijk', font= font, fill= 0)
-    draw.text((14, 48), 'lmnopqrsštuvwxyzžАБВГҐДЂ', font= font, fill= 0)
-    draw.text((14, 60), 'ЕГҐДЂЕЁЄЖЗЅИІЇЙЈКЛЉМН', font= font, fill= 0)
-    draw.text((14, 72), 'ЊОПРСТЋУЎФХЦЧЏШЩЪЫ', font= font, fill= 0)
-    draw.text((14, 84), 'ЬЭЮЯабвгґдђеёєжзѕиіїйјклљ', font= font, fill= 0)
-    draw.text((14, 96), 'мнњопрстћуўфхцчџшщъыьэ', font= font, fill= 0)
-    draw.text((14, 108), 'эюяΑΒΓΔΕΖΗΘΙΚΛΜΝΞΟΠΡΣΤ', font= font, fill= 0)
-    draw.text((14, 120), 'ΥΦΧΨΩαβγδεζηθικλμνξοπρστυ', font= font, fill= 0)
-    draw.text((14, 132), 'φψωĂÂÊÔƠƯăâêôơư12345678', font= font, fill= 0)
-    draw.text((14, 144), '9χ0‘?’“!”(%)[#]{@}/&\<-+÷×=>®©$', font= font, fill= 0)
-    draw.text((14, 156), '€£¥¢:;,.*', font= font, fill= 0)
+    text = ['ABCČĆDĐEFGHIJKLMNOPQR',
+            'SŠTUVWXYZŽabcčćdđefghijk',
+            'lmnopqrsštuvwxyzžАБВГҐДЂ',
+            'ЕГҐДЂЕЁЄЖЗЅИІЇЙЈКЛЉМН',
+            'ЊОПРСТЋУЎФХЦЧЏШЩЪЫ',
+            'ЬЭЮЯабвгґдђеёєжзѕиіїйјклљ',
+            'мнњопрстћуўфхцчџшщъыьэ',
+            'эюяΑΒΓΔΕΖΗΘΙΚΛΜΝΞΟΠΡΣΤ',
+            'ΥΦΧΨΩαβγδεζηθικλμνξοπρστυ',
+            'φψωĂÂÊÔƠƯăâêôơư12345678',
+            '9χ0‘?’“!”(%)[#]{@}/&\<-+÷×=>®©',
+            '$€£¥¢:;,.*']
+
+    for i in range(len(text)):
+        draw.text((8, 12 + 15*i), text[i], font= font, fill= 0)
+
     image.show()
     
     time.sleep(0.2)
 
     image = Image.open('../monocolor.bmp')
     image = image.resize(((EPD_WIDTH, EPD_HEIGHT)), Image.ANTIALIAS)
-    #image.show()
+    image.show()
 
 
 if __name__ == '__main__':
